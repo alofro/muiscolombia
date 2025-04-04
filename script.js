cefd95f6a01609dc2af83cdc008a29683094b834
alofro <anialopez@gmail.com>
2025-04-04 21:54:41 +0200

    debugging route

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Karte anzeigen
+// Funktion zum Anzeigen der Karte
 function showMap() {
   document.getElementById("map").style.display = "block";
   document.getElementById("elevationChart").style.display = "none";
 }
 
-// Höhenprofil anzeigen
+// Funktion zum Anzeigen des Höhenprofils
 function showElevation() {
   document.getElementById("map").style.display = "none";
   document.getElementById("elevationChart").style.display = "block";
@@ -13,6 +13,7 @@ function showElevation() {
 // Leaflet-Karte initialisieren
 const map = L.map('map').setView([4.7, -74.3], 10); // Zwischen Bogotá & Subía
 
+// OpenStreetMap als TileLayer verwenden
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap',
 }).addTo(map);
@@ -23,4 +24,7 @@ bogota.bindPopup('<b>Tag 1 Start</b><br>Bogotá');
 
 // Marker für Subía
 const subia = L.marker([4.75, -74.45]).addTo(map);
-subia.bindPopup('<b>Tag 1 Ziel</b><br>Sub
+subia.bindPopup('<b>Tag 1 Ziel</b><br>Subía');
+
+// OpenRouteService API-Aufruf
+const apiKey = '5b3ce3597851110001cf6248ef05ac1a70a6483086189e15a
