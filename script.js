731f9bec72233d72bba7f0e88972f5752320245c
alofro <anialopez@gmail.com>
2025-04-04 21:25:00 +0200

    Subía added

--- script.js
+++ script.js
@@ -18,8 +18,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Beispielmarker
-const marker = L.marker([4.711, -74.072]).addTo(map);
-marker.bindPopup('Start in Bogotá').openPopup();
+//const marker = L.marker([4.711, -74.072]).addTo(map);
+//marker.bindPopup('Start in Bogotá').openPopup();
+
+// Karte initialisieren
+const map = L.map('map').setView([4.6, -74.2], 9); // Zwischen Bogotá & Subía
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap',
+}).addTo(map);
+
+// Marker für Bogotá
+const bogota = L.marker([4.711, -74.072]).addTo(map);
+bogota.bindPopup('<b>Tag 1 Start</b><br>Bogotá');
+
+// Marker für Subía
+const subia = L.marker([4.75, -74.45]).addTo(map);
+subia.bindPopup('<b>Tag 1 Ziel</b><br>Subía');
+
+// Linie von Bogotá nach Subía
+const route1 = L.polyline([
+  [4.711, -74.072],
+  [4.75, -74.45]
+], {
+  color: 'blue'
+}).addTo(map);
+route1.bindPopup('Etappe 1: Bogotá → Subía');
+
 
 // Höhenprofil-Dummy
 const ctx = document.getElementById('elevationChart').getContext('2d');
