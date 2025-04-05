ce781564e82918db7a4a8500e9f0a3e5db535e41
alofro <anialopez@gmail.com>
2025-04-05 18:05:07 +0200

    natagaima added

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Definiere die Koordinaten für Bogotá und Subía
+// Definiere die Koordinaten für Bogotá, Subía, Natagaima
 var bogotaCoords = [-74.0721, 4.7110];  // Bogotá: [longitude, latitude]
 var subiaCoords = [-74.6250, 4.6167];  // Subía: [longitude, latitude]
+const natagaimaCoords = [4.1178, -75.5203];  // Koordinaten für Natagaima
 
 // Marker für Bogotá und Subía
 var bogotaMarker = L.marker([bogotaCoords[1], bogotaCoords[0]]).addTo(map);
 bogotaMarker.bindPopup("<strong>Bogotá</strong><br>Startpunkt");
+
+const natagaimaMarker = L.marker([natagaimaCoords[1], natagaimaCoords[0]]).addTo(map);
+natagaimaMarker.bindPopup("<b>Natagaima</b><br>Zwischenziel");
 
 var subiaMarker = L.marker([subiaCoords[1], subiaCoords[0]]).addTo(map);
 subiaMarker.bindPopup("<strong>Subía</strong><br>Zielpunkt");
@@ -25,6 +29,7 @@ var routeUrl = `https://api.openrouteservice.org/v2/directions/cycling-regular/g
 var routeRequestData = {
     coordinates: [
         [bogotaCoords[0], bogotaCoords[1]],  // Bogotá
+        [natagamiaCoords[0], natagamiaCoords[1]],     // Natagaima
         [subiaCoords[0], subiaCoords[1]]     // Subía
     ]
 };
