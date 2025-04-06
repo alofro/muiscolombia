29876469a4e382f55119fad5374025c1d4ccc9e7
Projekt Vogelhaus <projekt.vogelhaus@web.de>
2025-04-06 23:19:10 +0200

    debug

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ neivaMarker.bindPopup("<strong>Neiva</strong><br>Zwischenziel");
 var espinalMarker = L.marker([espinalCoords[1], espinalCoords[0]]).addTo(map);
 espinalMarker.bindPopup("<strong>Espinal</strong><br>Zwischenhalt");
 
-// Routenberechnung mit OpenRouteService API aus config.js
-var apiKey = 5b3ce3597851110001cf6248ef05ac1a70a6483086189e15a986bf78;  // API-Key aus der config.js
+// Routenberechnung mit OpenRouteService API 
+var apiKey = 5b3ce3597851110001cf6248ef05ac1a70a6483086189e15a986bf78;  // Mein API-Key 
 
 var routeUrl = `https://api.openrouteservice.org/v2/directions/driving-car/geojson?api_key=${apiKey}`;
 
