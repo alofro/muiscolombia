e8bf98e18997927c3b04c378143b1e368109a8ef
Projekt Vogelhaus <projekt.vogelhaus@web.de>
2025-04-06 22:38:48 +0200

    neiva added

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Definiere die Koordinaten für Bogotá, Subía, Natagaima
+// Definiere die Koordinaten, Bogotá, Subía, Natagaima
 var bogotaCoords = [-74.0787, 4.6459];  // Bogotá: [longitude, latitude]
 var subiaCoords = [-74.3834, 4.4709];  // Subia: [longitude, latitude]
 var natagaimaCoords = [-75.1002, 3.6266];  // Natagaima: [longitude, latitude]
+var neivaCoords = [-75.3116, 2.9541]; // Neiva: [longitude, latitude]
 
-// Marker für Bogotá und Subía
+// Marker für Bogotá, Subia, Natagaima, Neiva
 var bogotaMarker = L.marker([bogotaCoords[1], bogotaCoords[0]]).addTo(map);
 bogotaMarker.bindPopup("<strong>Bogotá</strong><br>Startpunkt");
+
+var subiaMarker = L.marker([subiaCoords[1], subiaCoords[0]]).addTo(map);
+subiaMarker.bindPopup("<strong>Subía</strong><br>Zwischenziel");
 
 var natagaimaMarker = L.marker([natagaimaCoords[1], natagaimaCoords[0]]).addTo(map);
 natagaimaMarker.bindPopup("<b>Natagaima</b><br>Zwischenziel");
 
-var subiaMarker = L.marker([subiaCoords[1], subiaCoords[0]]).addTo(map);
-subiaMarker.bindPopup("<strong>Subía</strong><br>Zielpunkt");
+var neivaMarker = L.marker([neivaCoords[1], neivaCoords[0]]).addTo(map);
+subiaMarker.bindPopup("<strong>Subía</strong><br>Zwischenziel");
 
 // Routenberechnung mit OpenRouteService API
 var apiKey = '5b3ce3597851110001cf6248ef05ac1a70a6483086189e15a986bf78'; // Dein OpenRouteService API-Key
@@ -29,8 +33,9 @@ var routeUrl = `https://api.openrouteservice.org/v2/directions/driving-car/geojs
 var routeRequestData = {
     coordinates: [
         [bogotaCoords[0], bogotaCoords[1]],     // Bogotá
-        [subiaCoords[0], subiaCoords[1]],        // Subía
-        [natagaimaCoords[0], natagaimaCoords[1]] // Natagaima
+        [subiaCoords[0], subiaCoords[1]],        // Subia
+        [natagaimaCoords[0], natagaimaCoords[1]], // Natagaima
+        [neivaCoords[0], neivaCoords[1]] // Neiva
     ]
 };
 
