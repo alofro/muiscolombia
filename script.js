2ba1b1ea18ad8e73a214a0dd26531e8214b8cff3
Projekt Vogelhaus <projekt.vogelhaus@web.de>
2025-04-08 20:16:54 +0200

    pinchada3 added

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ var natagaimaCoords = [-75.1002, 3.6266];  // Natagaima: [longitude, latitude]
 var pinchada2Coords = [-75.2441, 3.2398]; // Pinchada2
 var neivaCoords = [-75.3116, 2.9541]; // Neiva: [longitude, latitude]
 var espinalCoords = [-74.891551, 4.1501]; // Espinal [longitude, latitude]
+var pinchada3Coords = [-75.5201, 2.5238]; // Pinchada3
 var giganteCoords = [-75.5476, 2.386522]; // Gigante [longitude, latitude]
 var altamiraCoords = [-75.7895, 2.0650]; // Altamira [longitude, latitude]
 var timanaCoords = [-75.9326, 1.9723]; // Timaná
-
+var bruselasCoords = [-76.1703, 1.7714]; // Bruselas
 
 //Awesome Markers - spezielle Symbole für Marker
 var etapaMarker = L.AwesomeMarkers.icon({
@@ -94,6 +95,17 @@ espinalMarker.bindPopup(espinalContent);
 var neivaMarker = L.marker([neivaCoords[1], neivaCoords[0]], { icon: etapaMarker }).addTo(map);
 neivaMarker.bindPopup("<strong>Neiva</strong><br>Etapa 3");
 
+var pinchada3Marker = L.marker([pinchada3Coords[1], pinchada3Coords[0]], { icon: flatTireMarker }).addTo(map);
+var pinchada3Content = `
+  <div style="width: 200px;">
+    <img src="bilder/pinchada3.jpg" alt="Pinchada3" style="width: 100%; border-radius: 8px;">
+    <p style="font-size: 0.9em; margin-top: 5px;">
+      ¿?
+    </p>
+  </div>
+`;
+pinchada3Marker.bindPopup(pinchada3Content);
+
 var giganteMarker = L.marker([giganteCoords[1], giganteCoords[0]], { icon: etapaMarker }).addTo(map);
 giganteMarker.bindPopup("<strong>Gigante</strong><br>Etapa 4");
 
@@ -112,7 +124,8 @@ var timanaContent = `
 timanaMarker.bindPopup(timanaContent);
 
 
-
+var bruselasMarker = L.marker([bruselasCoords[1], bruselasCoords[0]], { icon: etapaMarker }).addTo(map);
+bruselasMarker.bindPopup("<strong>Bruselas</strong><br>Etapa 6");
 
 
 
@@ -134,7 +147,8 @@ var routeRequestData = {
         [neivaCoords[0], neivaCoords[1]], // Neiva
         [giganteCoords[0], giganteCoords[1]], // Gigante
         [altamiraCoords[0], altamiraCoords[1]], // Altamira
-        [timanaCoords[0], timanaCoords[1]] // Timana
+        [timanaCoords[0], timanaCoords[1]], // Timana
+        [bruselasCoords[0], bruselasCoords[1]] // Bruselas
         
     ]
 };
